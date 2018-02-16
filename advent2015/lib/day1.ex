f8fdaca4a981dcc2083deb2f310e689cee6084a2
Jonathan Ling <colonelpopcorn.jl@gmail.com>
2018-02-16 18:43:44 -0500

    Finish day 1 of advent 2015.

--- advent2015/lib/day1.ex
+++ advent2015/lib/day1.ex
@@ -4,7 +4,7 @@ defmodule Day1 do
   """
 
   @doc """
-  Hello world.
+  This will get santa through the building, and tell us what floor he ends up on. Takes tokens from input and increments numbers based on which one is read. Returns the increment.
 
   ## Examples
 
@@ -33,12 +33,40 @@ defmodule Day1 do
     |> increment(input, 0)
   end
 
-  defp increment(n, input, token) do
+  @doc """
+  This will get santa through the building, and . Takes tokens from input and increments numbers based on which one is read.
 
+  ## Examples
+
+      iex> Day1.secondary(")")
+      1
+      iex> Day1.secondary("()())")
+      5
+
+  """
+  def secondary(input) do
+    0
+    |> increment_secondary(input, 0)    
+  end
+
+  defp increment(n, input, token) do
     case String.slice(input, token..token) do
           ")" -> increment(n-1, input, token+1)
           "(" -> increment(n+1, input, token+1)
-          "" -> n            
-        end    
+          _ -> n            
+    end    
   end
+
+  defp increment_secondary(n, input, token) do
+    cond do
+       n < 0 -> token
+       true -> 
+      case String.slice(input, token..token) do
+        ")" -> increment_secondary(n-1, input, token+1)
+        "(" -> increment_secondary(n+1, input, token+1)
+        _ -> n            
+      end            
+    end   
+  end
+
 end
